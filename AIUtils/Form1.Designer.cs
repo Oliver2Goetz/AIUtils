d1fc5aada42434635a3d60db6ac60330f52822bf
Oliver Goetz <oli.goetz.o.g@gmail.com>
2022-12-11 11:32:30 +0100

    Basic form styling and functionality

--- AIUtils/Form1.Designer.cs
+++ AIUtils/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace AIUtils {
-    partial class Form1 {
+    partial class AIUtils {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,13 +23,60 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.pictureboxExit = new System.Windows.Forms.PictureBox();
+            this.labelHeadline = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureboxExit
+            // 
+            this.pictureboxExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureboxExit.Image = global::AIUtils.Properties.Resources.cross;
+            this.pictureboxExit.Location = new System.Drawing.Point(709, 12);
+            this.pictureboxExit.Name = "pictureboxExit";
+            this.pictureboxExit.Size = new System.Drawing.Size(40, 40);
+            this.pictureboxExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureboxExit.TabIndex = 0;
+            this.pictureboxExit.TabStop = false;
+            this.pictureboxExit.Click += new System.EventHandler(this.pictureboxExit_Click);
+            // 
+            // labelHeadline
+            // 
+            this.labelHeadline.AutoSize = true;
+            this.labelHeadline.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.labelHeadline.Location = new System.Drawing.Point(12, 15);
+            this.labelHeadline.Name = "labelHeadline";
+            this.labelHeadline.Size = new System.Drawing.Size(109, 37);
+            this.labelHeadline.TabIndex = 0;
+            this.labelHeadline.Text = "AIUtils";
+            // 
+            // AIUtils
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(761, 428);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelHeadline);
+            this.Controls.Add(this.pictureboxExit);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AIUtils";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AIUtils";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.AIUtils_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureboxExit;
+        private System.Windows.Forms.Label labelHeadline;
     }
 }
 
--- AIUtils/Form1.cs
+++ AIUtils/Form1.cs
@@ -7,11 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace AIUtils {
-    public partial class Form1 : Form {
-        public Form1() {
+
+    public partial class AIUtils : Form {
+
+        public AIUtils() {
             InitializeComponent();
         }
+
+
+
+
+        #region form
+
+        // Make window moveable
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void AIUtils_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        /*
+         * Exit form on click
+         */
+        private void pictureboxExit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        #endregion form
     }
 }
